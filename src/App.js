2fe0e663fe47fac936772319de3bcb24304fa64d
Ismael Perez <ismaelperezemail@gmail.com>
2020-07-20 09:55:15 -0700

    feat(Particles): added particle effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./App.css";
+import Particles from "react-particles-js";
 
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 800,
+            },
+        },
+    },
+};
+
 function App() {
     return (
         <div className="App">
+            <Particles params={particlesOptions} className="particles" />
             <Navigation />
             <Logo />
             <Rank />
