e0f3bffd061459af4f07f2b33fa7a9056da52b29
Ismael Perez <ismaelperezemail@gmail.com>
2020-07-19 19:29:53 -0700

    chore(App): basic app and component set up

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { findByLabelText } from "@testing-library/react";
+
+const Navigation = () => {
+    return (
+        <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+            <p>Sign Out</p>
+        </nav>
+    );
+};
+
+export default Navigation;
