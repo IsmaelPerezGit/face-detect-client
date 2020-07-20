75b587e45f268ab202e1487fecbb22d49af5b015
Ismael Perez <ismaelperezemail@gmail.com>
2020-07-20 10:13:23 -0700

    chore(ImageLinkForm): basic wire up of input and button

--- src/App.js
+++ src/App.js
@@ -19,17 +19,37 @@ const particlesOptions = {
     },
 };
 
-function App() {
-    return (
-        <div className="App">
-            <Particles params={particlesOptions} className="particles" />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-            {/*<FaceRecognition /> */}
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: "",
+        };
+    }
+
+    onInputChange = event => {
+        console.log(event.target.value);
+    };
+
+    onButtonSubmit = () => {
+        console.log("click");
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Particles params={particlesOptions} className="particles" />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
+                {/*<FaceRecognition /> */}
+            </div>
+        );
+    }
 }
 
 export default App;
