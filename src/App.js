0692d76482a5fc45598bee232b748046951531b6
Ismael Perez <ismaelperezemail@gmail.com>
2020-08-17 12:18:43 -0700

    feat(App): now detecting multiple faces

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const particlesOptions = {
 const initialState = {
     input: "",
     imageUrl: "",
-    box: {},
+    boxes: [],
     route: "signin",
     isSignedIn: false,
     user: {
@@ -43,7 +43,7 @@ class App extends React.Component {
         this.state = {
             input: "",
             imageUrl: "",
-            box: {},
+            boxes: [],
             route: "signin",
             isSignedIn: false,
             user: {
@@ -68,21 +68,22 @@ class App extends React.Component {
         });
     };
 
-    calculateFaceLocation = data => {
-        const clarifaiFace =
-            data.outputs[0].data.regions[0].region_info.bounding_box;
-        const image = document.getElementById("inputImage");
-        const width = Number(image.width);
-        const height = Number(image.height);
-        return {
-            leftCol: clarifaiFace.left_col * width,
-            topRow: clarifaiFace.top_row * height,
-            rightCol: width - clarifaiFace.right_col * width,
-            bottomRow: height - clarifaiFace.bottom_row * height,
-        };
+    calculateFaceLocations = data => {
+        return data.outputs[0].data.regions.map(face => {
+            const clarifaiFace = face.region_info.bounding_box;
+            const image = document.getElementById("inputImage");
+            const width = Number(image.width);
+            const height = Number(image.height);
+            return {
+                leftCol: clarifaiFace.left_col * width,
+                topRow: clarifaiFace.top_row * height,
+                rightCol: width - clarifaiFace.right_col * width,
+                bottomRow: height - clarifaiFace.bottom_row * height,
+            };
+        });
     };
 
-    displayFaceBox = box => this.setState({ box });
+    displayFaceBoxes = boxes => this.setState({ boxes });
     onInputChange = e => this.setState({ input: e.target.value });
 
     onButtonSubmit = () => {
@@ -117,7 +118,7 @@ class App extends React.Component {
                         })
                         .catch(console.log);
                 }
-                this.displayFaceBox(this.calculateFaceLocation(response));
+                this.displayFaceBoxes(this.calculateFaceLocations(response));
             })
             .catch(err => console.log(err));
     };
@@ -132,7 +133,7 @@ class App extends React.Component {
     };
 
     render() {
-        const { isSignedIn, imageUrl, route, box } = this.state;
+        const { isSignedIn, imageUrl, route, boxes } = this.state;
         return (
             <div className="App">
                 <Particles params={particlesOptions} className="particles" />
@@ -151,7 +152,7 @@ class App extends React.Component {
                             onInputChange={this.onInputChange}
                             onButtonSubmit={this.onButtonSubmit}
                         />
-                        <FaceRecognition box={box} imageUrl={imageUrl} />
+                        <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
                     </div>
                 ) : route === "signin" ? (
                     <Signin
